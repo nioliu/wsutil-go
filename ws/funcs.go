b4666aa0659d6815755d189d35c7d0e227538f8a
nioliu <nioliu@tencent.com>
2022-10-26 12:54:57 +0800

    如

--- ws/funcs.go
+++ ws/funcs.go
@@ -64,6 +64,8 @@ func (s *SingleConn) writePump() {
 				// todo add handle error func
 				utils.Logger.Error("send Msg failed", zap.Error(err))
 			}
+			// reset heart check if write successfully
+			ticker.Reset(s.heartCheck)
 
 			if s.afterHandleSendMsg != nil {
 				if err := s.afterHandleSendMsg(s.ctx, s.id, msgType, msg, TaskErrs); err != nil {
